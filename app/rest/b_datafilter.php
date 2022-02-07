22553480b3e41602d6ebf5390581114e18ac6fbe
jaleman <javiersettex@gmail.com>
2022-02-07 15:26:07 -0500

    filtro final

--- app/rest/b_datafilter.php
+++ app/rest/b_datafilter.php
@@ -41,17 +41,17 @@ function SELECT_DATA(){
 //               //
 ///////////////////
 function SELECT_DATA_ASESOR(){
-  $e = $_GET['e'];
-  $s = $_GET['s'];
   $a = $_GET['a'];
   date_default_timezone_set('America/Bogota');
   $hoy = date('Ymd');
   $obj = new conn;
-  $sql = "SELECT  `t_procesos`.`estado`, `t_procesos`.`sub`, `t_procesos`.`fgestion`, `t_procesos`.`operacion`, `t_asignacion`.`asesor` FROM `t_procesos` INNER JOIN `t_asignacion` ON `t_asignacion`.`operacion` = `t_procesos`.`operacion` WHERE `t_asignacion`.`asesor` = '$a' AND `t_procesos`.`fgestion` != '$hoy'  ORDER BY `t_procesos`.`fgestion` ASC LIMIT 1";
+
+  $sql = "SELECT  `t_procesos`.`estado`, `t_procesos`.`sub`, `t_procesos`.`fgestion`, `t_procesos`.`operacion`, `t_asignacion`.`asesor` FROM `t_procesos` INNER JOIN `t_asignacion` ON `t_asignacion`.`operacion` = `t_procesos`.`operacion` WHERE `t_asignacion`.`asesor` = '$a' AND `t_procesos`.`fgestion` != '$hoy'  ORDER BY `t_procesos`.`fgestion` ASC LIMIT 20";
   $con = $obj->query($sql);
   $num = mysqli_num_rows($con);
   $data['sql'] = $sql;
   $data['num'] = $num;
+  $data['tipo'] = 'asesor';
   if ($num >= 1) {
     while ($d = mysqli_fetch_assoc($con)) {
       $data['data'][] = array_map(CODING, $d);
@@ -71,11 +71,12 @@ function SELECT_DATA_ESTADO(){
   date_default_timezone_set('America/Bogota');
   $hoy = date('Ymd');
   $obj = new conn;
-  $sql = "SELECT  `t_procesos`.`estado`, `t_procesos`.`sub`, `t_procesos`.`fgestion`, `t_procesos`.`operacion`, `t_asignacion`.`asesor` FROM `t_procesos` INNER JOIN `t_asignacion` ON `t_asignacion`.`operacion` = `t_procesos`.`operacion` WHERE `t_asignacion`.`asesor` = '$a' AND `t_procesos`.`estado` = '$e' AND `t_procesos`.`sub` = '$s' AND `t_procesos`.`fgestion` != '$hoy' ORDER BY `t_procesos`.`fgestion` ASC LIMIT 1";
+  $sql = "SELECT  `t_procesos`.`estado`, `t_procesos`.`sub`, `t_procesos`.`fgestion`, `t_procesos`.`operacion`, `t_asignacion`.`asesor` FROM `t_procesos` INNER JOIN `t_asignacion` ON `t_asignacion`.`operacion` = `t_procesos`.`operacion` WHERE `t_asignacion`.`asesor` = '$a' AND `t_procesos`.`estado` = '$e' AND `t_procesos`.`sub` = '$s' AND `t_procesos`.`fgestion` != '$hoy' ORDER BY `t_procesos`.`fgestion` ASC ";
   $con = $obj->query($sql);
   $num = mysqli_num_rows($con);
   $data['num'] = $num;
   $data['sql'] = $sql;
+  $data['tipo'] = 'estado';
   if ($num >= 1) {
     while ($d = mysqli_fetch_assoc($con)) {
       $data['data'][] = array_map(CODING, $d);
@@ -94,11 +95,12 @@ function SELECT_DATA_CARTERA(){
   date_default_timezone_set('America/Bogota');
   $hoy = date('Ymd');
   $obj = new conn;
-  $sql = "SELECT `t_procesos`.`estado`, `t_procesos`.`sub`, `t_procesos`.`fgestion`, `t_procesos`.`operacion`, `t_asignacion`.`asesor`,`t_cartera`.`cartera` FROM `t_procesos` INNER JOIN `t_asignacion` ON `t_asignacion`.`operacion` = `t_procesos`.`operacion` INNER JOIN `t_cartera` ON `t_cartera`.`operacion` = `t_procesos`.`operacion` WHERE `t_asignacion`.`asesor` = '$a' AND `t_cartera`.`cartera` = '$c' AND `t_procesos`.`fgestion` != '$hoy' ORDER BY `t_procesos`.`fgestion` ASC LIMIT 1";
+  $sql = "SELECT `t_procesos`.`estado`, `t_procesos`.`sub`, `t_procesos`.`fgestion`, `t_procesos`.`operacion`, `t_asignacion`.`asesor`,`t_cartera`.`cartera` FROM `t_procesos` INNER JOIN `t_asignacion` ON `t_asignacion`.`operacion` = `t_procesos`.`operacion` INNER JOIN `t_cartera` ON `t_cartera`.`operacion` = `t_procesos`.`operacion` WHERE `t_asignacion`.`asesor` = '$a' AND `t_cartera`.`cartera` = '$c' AND `t_procesos`.`fgestion` != '$hoy' ORDER BY `t_procesos`.`fgestion` ASC ";
   $con = $obj->query($sql);
   $num = mysqli_num_rows($con);
   $data['num'] = $num;
   $data['sql'] = $sql;
+  $data['tipo'] = 'cartera';
   if ($num >= 1) {
     while ($d = mysqli_fetch_assoc($con)) {
       $data['data'][] = array_map(CODING, $d);
@@ -117,11 +119,12 @@ function SELECT_DATA_CAMPANA(){
   date_default_timezone_set('America/Bogota');
   $hoy = date('Ymd');
   $obj = new conn;
-  $sql = "SELECT `t_procesos`.`estado`, `t_procesos`.`sub`, `t_procesos`.`fgestion`, `t_procesos`.`operacion`, `t_asignacion`.`asesor`,`t_campana`.`campana` FROM `t_procesos` INNER JOIN `t_asignacion` ON `t_asignacion`.`operacion` = `t_procesos`.`operacion` INNER JOIN `t_campana` ON `t_campana`.`operacion` = `t_procesos`.`operacion`WHERE `t_asignacion`.`asesor` = '$a' AND `t_campana`.`campana` = '$c' AND `t_procesos`.`fgestion` != '$hoy' ORDER BY `t_procesos`.`fgestion` ASC LIMIT 1";
+  $sql = "SELECT `t_procesos`.`estado`, `t_procesos`.`sub`, `t_procesos`.`fgestion`, `t_procesos`.`operacion`, `t_asignacion`.`asesor`,`t_campana`.`campana` FROM `t_procesos` INNER JOIN `t_asignacion` ON `t_asignacion`.`operacion` = `t_procesos`.`operacion` INNER JOIN `t_campana` ON `t_campana`.`operacion` = `t_procesos`.`operacion`WHERE `t_asignacion`.`asesor` = '$a' AND `t_campana`.`campana` = '$c' AND `t_procesos`.`fgestion` != '$hoy' ORDER BY `t_procesos`.`fgestion` ASC ";
   $con = $obj->query($sql);
   $num = mysqli_num_rows($con);
   $data['num'] = $num;
   $data['sql'] = $sql;
+  $data['tipo'] = 'campana';
   if ($num >= 1) {
     while ($d = mysqli_fetch_assoc($con)) {
       $data['data'][] = array_map(CODING, $d);
